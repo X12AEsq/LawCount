a460827bd56680747dc44b40a8368573c88d1052
Morris Albers <applestore@alberslegal.com>
2024-08-24 11:03:53 -0500

    Maybe figuring out this whole Navigation Link thing

---
+++ LawCount/Views/SelectTransactionView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectTransactionView.swift
+//  LawCount
+//
+//  Created by Morris Albers on 8/24/24.
+//
+
+import SwiftUI
+
+struct SelectTransactionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectTransactionView()
+}
